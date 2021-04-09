f75d8b4db8d9db756f2adecc6ecb1843f3e78357
Fihra <fabrofabian@gmail.com>
2021-04-09 14:20:02 -0700

    continue working no history essay

--- src/components/History.js
+++ src/components/History.js
@@ -7,11 +7,22 @@ const History = () => {
             <legend><h2 id="history">History</h2></legend>
             <p>Rondalla originated in Spain, where later in the 16th century, the Spanish colonized Philippines, bringing Rondalla for the music culture. </p>
            
-            <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p> In 1565, Ferdinand Magellan arrived on the Philippine Islands. His main goal was to spread Christianity around the country. He also wanted to introduce the Spanish language and western European culture. 
+            </p>
+            <p>The Spanish church introduced music in the form of worship for Christian faith, with Gregorian chant and polyphony. The Spanish friars decided to teach the Filipino people Western music, from vocal and instrumental lessons to theory and counterpoint. 3 Types of music began cultivating from the Spanish culture: <i>harana</i>, <i>kundiman</i>, and <i>rondalla</i>. These were the results of combining elements of the Filipino indigenous music styles with Spanish music.
+            </p>
+            <ul>
+                <li><i>harana</i>: courting music (serenade) - Man sing/plays guitar for a woman beneath her window at nightfall. <i>habanera</i> influenced for its rhythmic elements. </li>
+                <li><i>kundiman</i>: sad, melancholy, blues music - Themes for hardships and heartbreaks, based on the Tagalog phrase, <i>"kung hindi man"</i>, which translates to "if not meant to be." Usually starts in minor key but modulates to its parallel major key.</li>
+                <li><i>rondalla</i>: plucked-string ensemble that contain mandolin-like instruments, guitars, and bass.</li>
+            </ul>
+            <p>Rondalla's origin of the name is theorized by Nitoy Gonzales, a former maestro of the Bayanihan Philippine Dancers. With <i>ronda</i>, meaning "guard," and <i>alla</i> meaning "over there." 
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <b>Quote from Justin Cydrick Gaffud Cabales's Thesis "Music of Pre-­‐‑colonial and Spanish Colonial Philippines, and the Filipino Rondalla":</b>
+            <blockquote>
+            "A bystander shouted, “A guard is there!” upon hearing a guard serenading a woman with his guitar, instead of protecting the barrio from the nearby threat of the Moslem pirates."    
+            </blockquote> 
+            </p>
             </p>
             </fieldset>
         </div>
